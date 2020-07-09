fcbf6ee66d550b68ca6d4c7a810b7ac96feed2b3
unais ali <51183141+unaisshazan@users.noreply.github.com>
2020-07-09 15:56:40 +0500

    add unais

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [repos, setRepos] = useState([{}]);
+
+  useEffect( () => {
+
+    async function getRepos() {
+      const response = await fetch("https://api.github.com/users/unaisshazan/repos")
+      const data = await response.json();
+      console.log(data);
+      setRepos(data)
+    }
+    getRepos();
+
+    // "https://api.github.com/users/muhammadmohsin/repos"
+    // fetch('https://jsonplaceholder.typicode.com/posts/1')
+    //   .then(response => response.json())
+    //   .then(json => {
+    //     console.log(json);
+    //     setData(json);
+    //   })
+  },[])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>You are seeing all repositories</h1>
+      <ul>
+        {repos.map((repoObj, ind) => {
+          return (<li key={ind}>{repoObj.name}</li>)
+        })}
+      </ul>
     </div>
   );
 }
